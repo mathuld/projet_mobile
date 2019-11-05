5363cdccc3ae750cc12c6d9b8d2f5b6fd0468272
Romain Guillot <rguillot42@gmail.com>
2019-11-05 17:29:51 -0500

    .

--- app/src/main/java/com/example/projet_mobile/GalleryView.java
+++ app/src/main/java/com/example/projet_mobile/GalleryView.java
@@ -2,10 +2,14 @@ package com.example.projet_mobile;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
 public class GalleryView extends View {
+
+    private float mScale = 1f;
+    private int mNbColumns = 3;
 
     private ScaleGestureDetector mScaleGestureDetector;
 
@@ -17,15 +21,43 @@ public class GalleryView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawBitmap();
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mScaleGestureDetector.onTouchEvent(event);
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
+            case MotionEvent.ACTION_MOVE:
+
+                invalidate();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+
+                invalidate();
+                break;
+        }
+        return true;
     }
 
     public class ScaleGesture extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
-            //mScale *= detector.getScaleFactor();
-            //mPaint.setTextSize(mScale*mFontSize);
+            mScale *= detector.getScaleFactor();
+
+            mScale = Math.max(mScale, 7);
+
+
+            System.out.println(mNbColumns);
+
             invalidate();
             return true;
         }
     }
+
+
 }
